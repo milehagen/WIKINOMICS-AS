47115e485c20a91495aacd77b6cd59a6c1a35093
Martin Johansen <s333727@oslomet.no>
2021-02-10 18:28:40 +0100

    Cookies are now made and JWT is stored

--- Bachelor/Controllers/UserController.cs
+++ Bachelor/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Bachelor.DAL;
 using Bachelor.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 
@@ -67,6 +68,19 @@ namespace Bachelor.Controllers
             string token = jwt.GenerateToken(userId);
             Console.WriteLine("JWT type: " + token.GetType() + "\n JWT tekst: " + token);
             Console.WriteLine("Token validate" + jwt.ValidateCurrentToken(token));
+
+
+            // Endre på senere - oppretter en cookie som var i 60 min og 30 sek
+            DateTimeOffset DtoUtchNow = DateTimeOffset.UtcNow;
+            TimeSpan Ts = TimeSpan.FromSeconds(30);
+
+            HttpContext.Response.Cookies.Append("<JWT>", token, new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                MaxAge = Ts
+            });
+
             return Ok(token);
         }
 
