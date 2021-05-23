1974a88c73a38c45fe7d436019dd0cc31763062c
Martin Johansen <s333727@oslomet.no>
2021-05-23 11:27:31 +0200

    edit experience looks different and now worksproperly. Errordialog now only shows a string

--- Bachelor/ClientApp/src/app/Profile/edit/profileEdit.component.ts
+++ Bachelor/ClientApp/src/app/Profile/edit/profileEdit.component.ts
@@ -23,6 +23,28 @@ export class ProfileEditComponent {
     public allIndustries : Array<Industry>;
     public allSubjects : Array<StudentSubject>;
     public user : User;
+
+    //new
+    private EditOccupation : boolean = false;
+    private EditIndustry : boolean = false;
+    private EditSubject : boolean = false;
+    private EditBusiness : boolean = false;
+    private EditStartDate : boolean = false;
+    private EditEndDate : boolean = false;
+
+    private showOccupationBusinessName : boolean = false;
+    private showSelectSubject : boolean = false;
+    private showSelectIndustry : boolean = false;
+
+    private occupationBusniessInput;
+    private selectedOccupation;
+    private selectedIndustry;
+    private selectedSubject;
+    private inputBusiness;
+    private inputStartDate;
+    private inputEndDate;
+
+    //
     
 
     constructor(private route : ActivatedRoute,
@@ -58,7 +80,7 @@ export class ProfileEditComponent {
         return this.formPatchExperience.get("endDate");
     }
 
-    Occupations: Array<Object> = [
+    Occupations : Array<Object>= [
         { id: 0, occupation: "Student" },
         { id: 1, occupation: "Full-time employee" },
         { id: 2, occupation: "Business owner" },
@@ -94,7 +116,7 @@ export class ProfileEditComponent {
         });
     }
 
-    async submit() {
+   /* async submit() {
         //First check if any of the values have changed
         const updateExperience = new Experience();
         updateExperience.id = this.experience.id;
@@ -141,8 +163,152 @@ export class ProfileEditComponent {
             this.router.navigate(['/profile/experience']);
         })
     }
+    */
 
     resetForm() {
         this.formPatchExperience.reset();
     }
+
+    async updateOccupation() {
+        if(this.selectedOccupation === undefined && typeof this.selectedOccupation == 'undefined') {
+            this.sharedService.openSnackBarMessage("You have to choose something in the menu", "Ok");
+            return;
+        }
+        const updatedExperience = this.experience;
+        updatedExperience.occupation = this.selectedOccupation.occupation;
+
+        if(updatedExperience.occupation === "Business owner") {
+            updatedExperience.business = this.occupationBusniessInput;
+        } else { updatedExperience.business = null; }
+
+        this.update(updatedExperience);
+        this.hideDivs();
+    }
+
+    openOccupationEdit() {
+        this.hideDivs();
+        this.EditOccupation = true;
+    }
+
+    onOccupationChange() {
+        if(this.selectedOccupation.occupation === 'Business owner') {
+            this.showOccupationBusinessName = true;
+        } else  { this.showOccupationBusinessName = false; }
+    }
+
+    async updateIndustry() {
+        if(this.selectedIndustry === undefined && typeof this.selectedIndustry == 'undefined') {
+            this.sharedService.openSnackBarMessage("You have to choose something in the menu", "Ok");
+            return;
+        }
+
+        const updatedExperience = this.experience;
+        updatedExperience.industry = this.selectedIndustry;
+        await this.update(updatedExperience);
+        this.selectedIndustry = undefined;
+        this.hideDivs(); 
+    }
+
+    openIndustryEdit() {
+        this.hideDivs();
+        this.EditIndustry = true;
+        console.log(this.experience);
+    }
+
+    async updateSubject() {
+        if(this.selectedSubject === undefined && typeof this.selectedSubject == 'undefined') {
+            this.sharedService.openSnackBarMessage("You have to choose something in the menu", "Ok");
+            return;
+        }
+
+        const updatedExperience = this.experience;
+        updatedExperience.studentSubject = this.selectedSubject;
+        await this.update(updatedExperience);
+        this.hideDivs();
+    }
+
+    openSubjectEdit() {
+        this.hideDivs();
+        this.EditSubject = true;
+    }
+
+    async updateBusiness() {
+        if(this.inputBusiness === undefined && typeof this.inputBusiness == 'undefined') {
+            this.sharedService.openSnackBarMessage("You have to choose something in the menu", "Ok");
+            return;
+        }
+
+        const updatedExperience = this.experience;
+        updatedExperience.business = this.inputBusiness;
+        await this.update(updatedExperience);
+        this.hideDivs();
+    }
+
+    openBusinessEdit()  {
+        this.hideDivs();
+        this.EditBusiness = true;
+    }
+
+    async updateStartDate() {
+        if(this.inputStartDate === undefined && typeof this.inputStartDate == 'undefined') {
+            this.sharedService.openSnackBarMessage("You have to choose something in the menu", "Ok");
+            return;
+        }
+
+        const updatedExperience = this.experience;
+        console.log(this.inputStartDate);
+        updatedExperience.startDate = this.inputStartDate;
+        await this.update(updatedExperience);
+        this.hideDivs();
+    }
+
+    async updateEndDate() {
+        if(this.inputEndDate === undefined && typeof this.inputEndDate == 'undefined') {
+            this.sharedService.openSnackBarMessage("You have to choose something in the menu", "Ok");
+            return;
+        }
+
+        const updatedExperience = this.experience;
+        updatedExperience.endDate = this.inputEndDate;
+        await this.update(updatedExperience);
+        this.hideDivs();
+    }
+
+    openDateEdit(value : number) {
+        this.hideDivs();
+        if(value === 0) {
+            this.EditStartDate = true;
+        } else {
+            this.EditEndDate = true;
+        }
+    }
+
+
+    //TODO
+    // FIKS REDIGERING AV TID OG FIKS EKSTRA OPPDATERING RUNDT OCCUPATION
+    //
+    hideDivs() {
+        this.EditBusiness = false;
+        this.EditEndDate = false;
+        this.EditIndustry = false;
+        this.EditOccupation = false;
+        this.EditStartDate = false;
+        this.EditSubject = false;
+    }
+
+    async update(exp : Experience) {
+        console.log(exp);
+        await this.userService.patchExperience(exp).then(res => {
+            if(res) {
+                this.sharedService.openSnackBarMessage("Erfaring redigert", "Ok");
+                this.userService.GetUser(this.user.id).then(updatedUser => {
+                this.sharedService.changeUser(updatedUser);
+                this.experience.user = updatedUser;
+               });
+            } else {
+                console.log("Fikk false");
+            }
+        });
+    }
+
 }
